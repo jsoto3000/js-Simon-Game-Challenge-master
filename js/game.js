8588f2087597039024c4b68a90ea2a280c4f3346
JS <jsoto3000@outlook.com>
2019-02-02 20:20:33 -0500

    initial commit with js and jquery

---
+++ js/game.js
@@ -0,0 +1,20 @@
+// alert("hello World");
+
+/*
+
+The great majority of web pages don't have any special
+requirements that would indicate moving the js to the top.
+Therefore, the case that usually works the best
+(js at the bottom) should be employed unless something
+comes up to invalidate the use of the default. Premature
+optimization is spending time worrying about speed. A
+default of putting js at the bottom requires not extra
+thought or effort.
+
+Top: When having JavaScript events function on elements
+immediately is more important (so if you use a DOM Ready
+event to load everything, this is the wrong place)
+
+Bottom: When loading the content is more important
+
+*/
